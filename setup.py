48ec4e2d1b77ba5bd85692783f87f5f32cb7667b
Rubber-Duck-999 <simon.crowther95@hotmail.co.uk>
2019-12-14 14:29:18 +0000

    Added jenkins pipeline script

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='IntegrationTest',
+      version='1.0',
+      description='A generic RabbitMQ Test consumer',
+      url='https://github.com/Rubber-Duck-999/HouseGuard-SystemProcessor',
+      author='Rubber-Duck-999',
+      packages=['IntegrationTest],
+      install_requires=['pika'],
+)
