212016fdf42e53e2773d81bfb641c9c8a85b5ff0
Simon Crowther <simon.crowther95@hotmail.co.uk>
2020-04-12 14:16:57 +0100

    Fix constants typo

--- src/system/constants.rs
+++ src/system/constants.rs
@@ -5,6 +5,6 @@ pub const FAULT_HANDLER: &str = "FH has died";
 pub const DATABASE_MANAGER: &str = "DBM has died";
 pub const USER_PANEL: &str = "UP has died";
 pub const COMPONENT_NAME: &str = "SYP has died";
-pub const SQL: &str = "SQL has died;"
+pub const SQL: &str = "SQL has died";
 pub const RABBITMQ: &str = "RMQ";
 pub const ERROR_FIND: u16 = 2500;
