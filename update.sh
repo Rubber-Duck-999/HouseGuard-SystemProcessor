07814192375cacf23ddbb9cc78c5a5409d1a7873
Rubber-Duck-999 <simon.crowther95@hotmail.co.uk>
2020-06-13 19:13:06 +0100

    Creation of update script

---
+++ update.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+cd $HOME/Documents/HouseGuard-SystemProcessor
+
+git pull
+success=$?
+echo $success
+
+#cargo clean
+
+#cargo build
+
+if [ -f target/debug/exeSystemProcessor ];
+then
+    echo "SYS File found"
+    if [ -f $HOME/Documents/Temp/exeSystemProcessor ];
+    then
+        echo "SYS old removed"
+        rm -f $HOME/Documents/Temp/exeSystemProcessor
+    fi
+    cp target/debug/exeSystemProcessor $HOME/Documents/Temp/exeSystemProcessor
+fi
